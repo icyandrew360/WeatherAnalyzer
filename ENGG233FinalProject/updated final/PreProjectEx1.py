efa0a525037b3d34dad20053db91906420f2c2ac
icyandrew360 <100974845+icyandrew360@users.noreply.github.com>
2022-12-17 00:26:23 -0700

    Delete ENGG233FinalProject directory

--- ENGG233FinalProject/updated final/PreProjectEx1.py
+++
@@ -1,9 +0,0 @@
-#Code by Andrew Howe and John Manoli
-
-import numpy as np
-
-array = np.random.randint(100, size = 10)
-
-print (array)
-
-print ("The min is {} \nthe max is {} \nthe average is {} \nthe median is {} \nthe standard deviation is {}.".format (np.min(array), np.max(array), np.average(array), np.median(array), np.std(array)))
